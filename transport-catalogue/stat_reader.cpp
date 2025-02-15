acaf4a507e99e9c036a8491676007c5c83173347
Anton Akulov <zantshark@gmail.com>
2025-02-15 22:48:51 +0000

    Version 1.0 base

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,56 @@
-// место для вашего кода
+#include <iostream>
+#include <string>
+#include <set>
+
+#include "stat_reader.h"
+
+using namespace std::literals;
+using namespace transport_catalogue;
+
+void PrintBus (const TransportCatalogue& tansport_catalogue, std::string_view request, std::ostream& output, 
+               std::string_view command_id) {
+    const auto& bus = tansport_catalogue.FindBus(command_id);
+    if (bus == nullptr) {
+        output << request << ": not found"s << std::endl;
+        return;
+    }
+    const TransportCatalogue::RouteInfo route = tansport_catalogue.GetRouteInfo(bus);
+    output << request << ": "s << route.stops_number << " stops on route, "s 
+        << route.unique_stops_number << " unique stops, "s 
+        << route.distance << " route length"s << std::endl;
+}
+
+void PrintStop (const TransportCatalogue& tansport_catalogue, std::string_view request, std::ostream& output, 
+                std::string_view command_id) {
+    if (tansport_catalogue.FindStop(command_id) == nullptr) {
+        output << request << ": not found"s << std::endl;
+        return;
+    }
+    const std::set<std::string_view>& buses = tansport_catalogue.GetBusesToStop(command_id);
+    if (buses.empty()) {
+        output << request << ": no buses"s << std::endl;
+        return;
+    }
+    output << request << ": buses"s;
+    for (const std::string_view bus : buses) {
+        output << " "s << bus;
+    }
+    output << std::endl;
+}
+
+void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
+                       std::ostream& output) {
+    auto space_pos = request.find(' ');
+    if (space_pos == request.npos) {
+        return;
+    }
+    const std::string_view command = request.substr(0, space_pos);
+    const std::string_view command_id = request.substr(space_pos + 1);
+
+    if (command == "Bus") {
+        PrintBus(tansport_catalogue, request, output, command_id);
+    }
+    if (command == "Stop") {
+        PrintStop(tansport_catalogue, request, output, command_id);
+    }
+}
--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,47 @@
-// место для вашего кода
+#pragma once
+
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <vector>
+
+#include "geo.h"
+
+namespace transport_catalogue {
+
+class TransportCatalogue {
+public:
+	struct Bus {
+    	std::string name;
+    	std::vector<std::string> stops;
+	};
+
+	struct Stop {
+    	std::string name;
+    	geo::Coordinates coordinates;
+	};
+
+	struct RouteInfo {
+    	size_t stops_number = 0;
+    	size_t unique_stops_number = 0;
+    	double distance = 0;
+	};
+
+	void AddStop(const std::string name, const geo::Coordinates coordinates);
+	void AddBus(const std::string name, const std::vector<std::string> stops);
+	const Bus* FindBus(const std::string_view name) const;
+	const Stop* FindStop(const std::string_view name) const;
+	const std::set<std::string_view> GetBusesToStop(const std::string_view stop_name) const;
+	const RouteInfo GetRouteInfo(const Bus* bus) const;
+
+private:
+	std::deque<Stop> all_stops_;
+    std::deque<Bus> all_buses_;
+
+	std::unordered_map<std::string_view, const Stop*> name_to_stop_;
+    std::unordered_map<std::string_view, const Bus*> name_to_bus_;
+	std::unordered_map<std::string_view, std::set<std::string_view>> stop_to_buses_;
+};
+
+}
