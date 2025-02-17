77f5cda78ef2b5ae6785ecfff8a5c6c16b7a7ca0
Anton Akulov <zantshark@gmail.com>
2025-02-17 20:08:21 +0000

    offset fixed

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -28,20 +28,20 @@ struct RouteInfo {
 
 class TransportCatalogue {
 public:
-	void AddStop(const std::string& name, const geo::Coordinates coordinates);
-	void AddBus(const std::string& name, const std::vector<std::string_view> stops);
-	const Bus* FindBus(const std::string_view name) const;
-	const Stop* FindStop(const std::string_view name) const;
+    void AddStop(const std::string& name, const geo::Coordinates coordinates);
+    void AddBus(const std::string& name, const std::vector<std::string_view> stops);
+    const Bus* FindBus(const std::string_view name) const;
+    const Stop* FindStop(const std::string_view name) const;
     const std::unordered_set<std::string_view>& GetBusesToStop(const std::string_view stop_name) const;
-	const RouteInfo GetRouteInfo(const Bus* bus) const;
+    const RouteInfo GetRouteInfo(const Bus* bus) const;
 
 private:
-	std::deque<Stop> all_stops_;
+    std::deque<Stop> all_stops_;
     std::deque<Bus> all_buses_;
 
-	std::unordered_map<std::string_view, const Stop*> name_to_stop_;
+    std::unordered_map<std::string_view, const Stop*> name_to_stop_;
     std::unordered_map<std::string_view, const Bus*> name_to_bus_;
-	std::unordered_map<std::string_view, std::unordered_set<std::string_view>> stop_to_buses_;
+    std::unordered_map<std::string_view, std::unordered_set<std::string_view>> stop_to_buses_;
 };
 
 }
